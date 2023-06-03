97c78e44749aec9f92be3675d6da983b89d965bd
Markus Heinemann <markus.heinemann@trustify.ch>
2023-06-03 23:13:26 +0200

    Initial Commit

---
+++ Sensorik/SensorikApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorikApp.swift
+//  Sensorik
+//
+//  Created by Markus on 03.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SensorikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
